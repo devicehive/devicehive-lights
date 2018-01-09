163cf6cf45c7dd5f5bab418727ae00f6627572c8
Eugene Dubovik <edubovik90@gmail.com>
2018-01-09 17:43:11 +0200

    Updated lib version;

--- things/src/main/java/com/devicehive/lights/tools/CommandService.java
+++ things/src/main/java/com/devicehive/lights/tools/CommandService.java
@@ -34,19 +34,21 @@ import com.github.devicehive.client.model.FailureData;
 import com.github.devicehive.client.service.Device;
 import com.github.devicehive.client.service.DeviceCommand;
 import com.github.devicehive.client.service.DeviceHive;
-import com.github.devicehive.rest.model.JsonStringWrapper;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 public class CommandService {
     //TODO to make this application work set URL, REFRESH_TOKEN and DEVICE_ID
@@ -65,7 +67,16 @@ public class CommandService {
     private WS2812B strip;
     private ScheduledFuture<?> future;
     private ScheduledExecutorService scheduledExecutorService;
-    private ExecutorService service;
+    private Observable<DeviceCommand> observable;
+
+    public static CommandService getInstance() {
+        return CommandService.InstanceHolder.INSTANCE;
+    }
+
+    private static class InstanceHolder {
+        static final CommandService INSTANCE = new CommandService();
+    }
+
 
     private CommandService() {
         if (TextUtils.isEmpty(URL)) {
@@ -77,88 +88,84 @@ public class CommandService {
         if (TextUtils.isEmpty(DEVICE_ID)) {
             throw new RuntimeException("URL cannot be null or empty");
         }
-
         deviceHive = DeviceHive.getInstance().init(URL, REFRESH_TOKEN);
+        deviceHive.enableDebug(true);
         strip = new WS2812B();
-        service = Executors.newFixedThreadPool(1);
         scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        createCommandSubscription();
     }
 
-    public static CommandService getInstance() {
-        return CommandService.InstanceHolder.INSTANCE;
-    }
-
-    private static class InstanceHolder {
-        static final CommandService INSTANCE = new CommandService();
-    }
-
-    public void start() {
-        service.submit(new Runnable() {
-            @Override
-            public void run() {
-                DHResponse<Device> deviceDHResponse = deviceHive.getDevice(DEVICE_ID);
-                if (!deviceDHResponse.isSuccessful()) {
-                    return;
-                }
-                Device device = deviceDHResponse.getData();
-                CommandFilter commandFilter = new CommandFilter();
-                commandFilter.setCommandNames(LIGHTS_BLINKER, LIGHTS_RANDOM, LIGHTS_AUDIO, LIGHTS_OFF);
-
-
-                device.subscribeCommands(commandFilter, new DeviceCommandsCallback() {
-                    @Override
-                    public void onSuccess(List<DeviceCommand> list) {
-                        DeviceCommand command = list.get(0);
-                        if (Objects.equals(command.getCommandName(), LIGHTS_AUDIO)) {
-                            if (future != null) {
-                                future.cancel(true);
-                            }
-
-                            future = getlights(command.getParameters());
-
-                        } else if (Objects.equals(command.getCommandName(), LIGHTS_BLINKER)) {
-                            if (future != null) {
-                                future.cancel(true);
-                            }
-                            future = getBlinker();
-
-
-                        } else if (Objects.equals(command.getCommandName(), LIGHTS_RANDOM)) {
-                            if (future != null) {
-                                future.cancel(true);
-                            }
-                            future = getRandom();
-
-                        } else if (Objects.equals(command.getCommandName(), LIGHTS_OFF)) {
-                            if (future != null) {
-                                future.cancel(true);
-                            }
-                            try {
-                                Thread.sleep(100);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            strip.offLed();
-                        }
-                    }
-
-                    @Override
-                    public void onFail(FailureData failureData) {
-                        if (future != null) {
-                            future.cancel(true);
-                        }
-                        strip.offLed();
-                    }
-                });
-                while (true) {
-                    //special for TeamLead
-                }
+    private void createCommandSubscription() {
+        observable = Observable.create(e -> {
+            DHResponse<Device> deviceDHResponse = deviceHive.getDevice(DEVICE_ID);
+            if (!deviceDHResponse.isSuccessful()) {
+                return;
             }
+            Device device = deviceDHResponse.getData();
+            CommandFilter commandFilter = new CommandFilter();
+            commandFilter.setCommandNames(LIGHTS_BLINKER, LIGHTS_RANDOM, LIGHTS_AUDIO, LIGHTS_OFF);
+
+            device.subscribeCommands(commandFilter, new DeviceCommandsCallback() {
+                @Override
+                public void onSuccess(List<DeviceCommand> list) {
+                    DeviceCommand command = list.get(0);
+                    e.onNext(command);
+                }
+
+                @Override
+                public void onFail(FailureData failureData) {
+                    e.onError(new Throwable(failureData.getMessage()));
+                }
+            });
         });
     }
 
-    public void stop() {
-        service.shutdownNow();
+    public void start() {
+        observable.subscribeOn(Schedulers.computation())
+                .observeOn(Schedulers.computation())
+                .subscribe(command -> {
+                    if (Objects.equals(command.getCommandName(), LIGHTS_AUDIO)) {
+                        if (future != null) {
+                            future.cancel(true);
+                        }
+                        future = getLights(command.getParameters());
+                    } else if (Objects.equals(command.getCommandName(), LIGHTS_BLINKER)) {
+                        if (future != null) {
+                            future.cancel(true);
+                        }
+                        future = getBlinker();
+
+                    } else if (Objects.equals(command.getCommandName(), LIGHTS_RANDOM)) {
+                        if (future != null) {
+                            future.cancel(true);
+                        }
+                        future = getRandom();
+
+                    } else if (Objects.equals(command.getCommandName(), LIGHTS_OFF)) {
+                        if (future != null) {
+                            future.cancel(true);
+                        }
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        strip.offLed();
+                        stop();
+                    }
+                }, throwable -> {
+                    System.out.println("onError");
+                    if (future != null) {
+                        future.cancel(true);
+                    }
+                    strip.offLed();
+                    stop();
+                });
+    }
+
+    private void stop() {
+        if (observable != null)
+            observable.doOnComplete(() -> System.out.println("onComplete"));
     }
 
     private ScheduledFuture<?> getBlinker() {
@@ -169,35 +176,31 @@ public class CommandService {
         }
 
         strip.commit(colors);
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                if (params.getIter() <= 30) {
-                    params.iterateForward();
-                    colors.set(params.getLeft(), Color.valueOf(Color.WHITE));
-                    colors.set(params.getRight(), Color.valueOf(Color.RED));
+        Runnable r = () -> {
+            if (params.getIter() <= 30) {
+                params.iterateForward();
+                colors.set(params.getLeft(), Color.valueOf(Color.WHITE));
+                colors.set(params.getRight(), Color.valueOf(Color.RED));
 
-                } else if (params.getIter() > 30 && params.getIter() < 62) {
-                    colors.set(params.getLeft(), Color.valueOf(Color.BLACK));
-                    colors.set(params.getRight(), Color.valueOf(Color.BLACK));
-                    params.iterateBack();
-                } else if (params.getIter() == 62) {
-                    params.setDefaults();
-                }
-                strip.commit(colors);
+            } else if (params.getIter() > 30 && params.getIter() < 62) {
+                colors.set(params.getLeft(), Color.valueOf(Color.BLACK));
+                colors.set(params.getRight(), Color.valueOf(Color.BLACK));
+                params.iterateBack();
+            } else if (params.getIter() == 62) {
+                params.setDefaults();
             }
+            strip.commit(colors);
         };
         return scheduledExecutorService.scheduleAtFixedRate(r, 50, 50, TimeUnit.MILLISECONDS);
     }
 
-    private ScheduledFuture<?> getlights(JsonStringWrapper wrapper) {
-        System.out.println(wrapper.getJsonString());
+    private ScheduledFuture<?> getLights(JsonObject jsonObject) {
         int speed = 300;
         Gson gson = new Gson();
         Type listType = new TypeToken<List<ColorParam>>() {
         }.getType();
 
-        final ColorJson colorParam = gson.fromJson(wrapper.getJsonString(), ColorJson.class);
+        final ColorJson colorParam = gson.fromJson(jsonObject.toString(), ColorJson.class);
         final LinkedList<ColorParam> colorList = new LinkedList<>();
         try {
             List<ColorParam> colorListJson = gson.fromJson(colorParam.getColors(), listType);
@@ -212,12 +215,9 @@ public class CommandService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                colorList.addFirst(colorList.pollLast());
-                strip.commit(colorList);
-            }
+        Runnable r = () -> {
+            colorList.addFirst(colorList.pollLast());
+            strip.commit(colorList);
         };
 
         return scheduledExecutorService.scheduleAtFixedRate(r, 10, speed, TimeUnit.MILLISECONDS);
@@ -225,12 +225,9 @@ public class CommandService {
 
     private ScheduledFuture<?> getRandom() {
         final LinkedList<Color> colors = WS2812B.generateRandomColors(300);
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                colors.addFirst(colors.pollLast());
-                strip.commit(colors);
-            }
+        Runnable r = () -> {
+            colors.addFirst(colors.pollLast());
+            strip.commit(colors);
         };
 
         return scheduledExecutorService.scheduleAtFixedRate(r, 50, 100, TimeUnit.MILLISECONDS);
